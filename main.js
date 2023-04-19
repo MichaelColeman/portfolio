900430736b7598fe18bc00e74c9c71d9c2802275
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-04-19 09:16:00 -0700

    change to use fetch instead of the sdk

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
 //bind to submit button
-const submitButton = document.getElementById('send-email');
-if (!submitButton) { console.warn("submit button not found") };
-console.log(submitButton);
+const submitButton = document.getElementById('send-email')
+if (!submitButton) {
+  console.warn('submit button not found')
+}
 
 //bind to form
-const form = document.getElementById('formjs');
+const form = document.getElementById('formjs')
 
 //add form submit event listener to form
 if (!form) {
-  console.warn("form not found");
+  console.warn('form not found')
 } else {
   form.addEventListener('submit', (event) => {
-    event.preventDefault();
-  });
-};
+    event.preventDefault()
+    // console.log(event)
+    submitForm(form)
+  })
+}
 
-console.log(form);
+async function submitForm(form) {
+  //construct the form
+  const data = new FormData(form)
+  data.append('service_id', 'default_service')
+  data.append('template_id', 'template_j9drx49')
+  data.append('user_id', 'OfCYmZsIy-mQT7yaE')
+  for (const value of data.values()) {
+    console.log(value)
+  }
 
+  //send the form
+  // try {
+  //   const response = await fetch('https://api.emailjs.com/api/v1.0/email/send-form', {
+  //     method: 'POST',
+  //     body: data,
+  //   })
+  //
+  //   console.log(response.status)
+  // } catch (error) {
+  //   console.log(`fetch?: ${error}`)
+  // }
 
-
-
-
-// function init(formID) {
-//   const form = document.getElementById(formID)
-//   if (!form) {
-//     console.log('Form not found')
-//     return
-//   }
-//   console.log('Form Found')
-
-//   form.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     sendMail(form)
-//   })
-// }
-
-// function sendMail(form) {
-//   console.log('sendMail()')
-//   emailjs.sendForm('default_service', 'template_j9drx49', form, 'OfCYmZsIy-mQT7yaE').then(
-//     (res) => {
-//       console.log('res', res)
-//     },
-//     (err) => {
-//       console.log('err', err)
-//     }
-//   )
-// }
-
-// init('formjs')
+  const status = 200
+  if (status === 200) {
+    // on success, indicate to the user that the message sent was sucessful
+    console.log('success')
+    submitButton.style.backgroundColor = 'green'
+  } else {
+    // on failure (400 respose or similar), indicate to the user that the message send was a fail
+    console.log('fail')
+  }
+}
